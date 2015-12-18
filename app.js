c00ba37ed0e2075507a5d15d442bf32962f7fc6e
Prashant Chaudhari <prashant.chaudhari89@gmail.com>
2015-12-18 16:06:16 +0530

    use fs and ejs

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
 var http = require('http');
 var dispatcher = require('httpdispatcher');
 
+var fs = require('fs');
+var ejs = require('ejs');
 //Lets define a port we want to listen to
 const PORT=8080; 
 
 
 //For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('resources');
+dispatcher.setStatic('public');
 
 //A sample GET request    
 dispatcher.onGet("/page1", function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Page One');
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  //res.end('Page One');
+  var templateString = null;
+  var templateString = fs.readFileSync('pages/index.ejs', 'utf-8');
+  res.end(ejs.render(templateString));
 });    
 
 //A sample POST request
